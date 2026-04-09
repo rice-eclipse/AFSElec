1c294c3d8ba04e6c5486aff488225a9a08d58431
Aayan Imran Ilyas <136484628+Ion2007@users.noreply.github.com>
2026-04-09 18:35:21 -0500

    ChangesFUCKKKKKKKKKKKKKKKKK

--- Code/workingCode/library_test/test_ICM42688/test_ICM42688.ino
+++ Code/workingCode/library_test/test_ICM42688/test_ICM42688.ino
@@ -1,47 +1,29 @@
 #include "IMU.h"
+#include <SPI.h>
 
-// Pin definitions
 #define IMU_CS 13
-#define SPI_SCK 10
-#define SPI_SDI 8
-#define SPI_SDO 11
 
-// Create ICM42688 instance with SPI1 and CS pin 13
-ICM42688 imu(SPI1, IMU_CS, ACCEL_8G, GYRO_1000DPS);
+ICM42688 imu(SPI, IMU_CS, ACCEL_8G, GYRO_1000DPS);
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     delay(1000);
 
-    Serial.println("ICM42688 Library Test");
-    Serial.println("=====================");
+    SPI.begin();
 
-    // Configure SPI pins
-    SPI1.setSCK(SPI_SCK);
-    SPI1.setTX(SPI_SDO);
-    SPI1.setRX(SPI_SDI);
-    SPI1.begin();
+    pinMode(IMU_CS, OUTPUT);
+    digitalWrite(IMU_CS, HIGH);
 
-    // Initialize IMU
-    if (imu.begin())
-    {
-        Serial.println("✓ IMU initialized successfully");
+    if (imu.begin()) {
+        Serial.println("IMU initialized successfully");
+    } else {
+        Serial.println("IMU initialization failed");
     }
-    else
-    {
-        Serial.println("✗ IMU initialization failed");
-    }
-
-    Serial.println();
 }
 
-void loop()
-{
-    // Read sensor data
+void loop() {
     imu.readAll();
 
-    // Print accelerometer data
     Serial.print("Accel: ");
     Serial.print(imu.getAccelX(), 2);
     Serial.print(", ");
@@ -49,7 +31,6 @@ void loop()
     Serial.print(", ");
     Serial.print(imu.getAccelZ(), 2);
 
-    // Print gyroscope data
     Serial.print(" | Gyro: ");
     Serial.print(imu.getGyroX(), 2);
     Serial.print(", ");
@@ -57,7 +38,6 @@ void loop()
     Serial.print(", ");
     Serial.print(imu.getGyroZ(), 2);
 
-    // Print temperature
     Serial.print(" | Temp: ");
     Serial.print(imu.getTemp(), 2);
     Serial.println(" C");
